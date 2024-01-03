a289c96d0f45f91fcbd030d5958ac76da578855d
hassanhexa <132433319+hassanhexa@users.noreply.github.com>
2024-01-03 16:01:02 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,18 +1,15 @@
-// Create an array to store participants
-const participantsList = [];
-
 function generateCountry() {
-    const nameInput = document.getElementById('name').value.toLowerCase();
+    const nameInput = document.getElementById('name').value;
     const friends = {
-        hassan: 'JANNAT',
-        arslan: 'SAUDIA ARABIA',
-        hamza: 'NORWAY',
-        jahanzaib: 'MEXICO',
-        ruman: 'AFGHANISTAN',
-        khizar: 'PALESTINE',
-        hammad: 'INDIA',
-        osama: 'USA',
-        shajee: 'IRAN',
+        HASSAN: 'JANNAT',
+        ARSLAN: 'SAUDIA ARABIA',
+        HAMZA: 'NORWAY',
+        JAHANZAIB: 'MEXICO',
+        RUMAN: 'AFGHANISTAN',
+        KHIZAR: 'PALESTINE',
+        HAMMAD: 'INDIA',
+        OSAMA: 'USA',
+        SHAJEE: 'IRAN',
     };
 
     // Check if the entered name is in the friends list
@@ -24,14 +21,8 @@ function generateCountry() {
         const friendNameElement = document.getElementById('friendName');
         const countryNameElement = document.getElementById('countryName');
         friendNameElement.innerText = `${nameInput}, your fate awaits...`;
-        countryNameElement.innerText = `You might end up in ${country}!`;
+        countryNameElement.innerText = `You Will end up in ${country}!`;
         popup.style.display = 'block';
-
-        // Add the participant to the list
-        participantsList.push({ name: nameInput, country });
-
-        // Update the participants list on the page
-        updateParticipantsList();
     } else {
         // Display an error message if the name is not found
         alert('Sorry, the name is not in the list of friends.');
@@ -41,16 +32,4 @@ function generateCountry() {
 function closePopup() {
     const popup = document.getElementById('popup');
     popup.style.display = 'none';
-}
-
-// Function to update the participants list on the page
-function updateParticipantsList() {
-    const participantsListElement = document.getElementById('participantsList');
-    participantsListElement.innerHTML = '';
-
-    participantsList.forEach((participant) => {
-        const listItem = document.createElement('li');
-        listItem.innerText = `${participant.name}: ${participant.country}`;
-        participantsListElement.appendChild(listItem);
-    });
 }
